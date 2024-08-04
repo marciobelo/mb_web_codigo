714771c8677356a416e3f5d74eac2b97da9c299b
Márcio Belo <mbelo.br@gmail.com>
2024-08-04 20:51:46 -0300

    codigo do consulta produto

---
+++ bazar/buscaProduto.php
@@ -0,0 +1,19 @@
+<?php
+$banco_em_memoria = array(
+    1 => array(
+        "descricao" => "Marreta Para Quebrar Concreto",
+        "preco" => 123.45
+    )
+    // imagine centenas de produtos aqui!
+);
+
+$codigo = filter_input( INPUT_GET, "codigo", FILTER_SANITIZE_NUMBER_INT);
+
+$produto = $banco_em_memoria[ $codigo ];
+
+if( $produto == null) {
+    http_response_code(404); // NOT FOUND
+} else {
+    Header("Content-Type: application/json");
+    echo json_encode($produto);
+}
