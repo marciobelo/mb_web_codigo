5789f6779323e02d0d3f97a14cd5be5882d21db8
Márcio Belo <mbelo.br@gmail.com>
2024-08-17 10:16:08 -0300

    Esqueleto para o exercicio SenhaForte

---
+++ senha-forte/senha-forte.js
@@ -0,0 +1,10 @@
+$(function() {
+
+    $("#criarConta").click(function() {
+
+        // codigo de exemplo para altenar exibicao
+        $("#LoginAoMenos6Ok").css({display: 'inline'});
+        $("#LoginAoMenos6Nok").css({display: 'none'});
+
+    });
+});
